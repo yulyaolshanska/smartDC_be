5e355bd3d3b9b80840ee90c1a075db0a369b8676
DaniilVolobuiev <danalex999@ukr.net>
2023-04-07 16:11:50 +0200

    resolving conflicts

--- src/modules/doctor/doctor.controller.ts
+++ src/modules/doctor/doctor.controller.ts
@@ -7,35 +7,4 @@ import Doctor from './entity/doctor.entity';
 @Controller('create_doctor')
 export default class DoctorController {
   constructor(private readonly doctorService: DoctorService) {}
-
-  @ApiOperation({ summary: 'Doctor creation' })
-  @ApiResponse({ status: 201, type: Doctor })
-  @Post()
-  createDoctor(
-    @Body() doctorDto: CreateDoctorDto,
-    link: string,
-  ): Promise<Doctor> {
-    return this.doctorService.createDoctor(doctorDto, link);
-  }
-
-  @ApiOperation({ summary: 'Getting all doctors' })
-  @ApiResponse({ status: 200, type: [Doctor] })
-  @Get()
-  getAll(): Promise<Doctor[]> {
-    return this.doctorService.getAllDoctors();
-  }
-
-  @ApiOperation({ summary: 'Getting doctor by id' })
-  @ApiResponse({ status: 200, type: Doctor })
-  @Get('/:id')
-  getOne(@Param('id') id: number): Promise<Doctor> {
-    return this.doctorService.getDoctorByID(id);
-  }
-
-  @ApiOperation({ summary: 'Delete doctor by id' })
-  @ApiResponse({ status: 200, type: [Doctor] })
-  @Delete('/:id')
-  deleteOne(@Param('id') id: number): Promise<void> {
-    return this.doctorService.deleteDoctorById(id);
-  }
 }
