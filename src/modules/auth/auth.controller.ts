a0d82b818c8d3175a49216768b7c68bea8fcc2c6
Olena Posternak <alena.kavyrshyna@gmail.com>
2023-05-23 16:01:10 +0300

    swagger

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -42,6 +42,8 @@ class AuthController {
     return this.authService.registration(doctorDto);
   }
 
+  @ApiOperation({ summary: 'Get doctor with new activation link' })
+  @ApiResponse({ status: 201, type: Doctor })
   @Patch('/reactivation/:id')
   updateOne(@Param('id') id: number): Promise<Doctor> {
     return this.authService.sendActivationLink(id);
